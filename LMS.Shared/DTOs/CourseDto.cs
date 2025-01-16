f2a78c85abe52c0658c305b9732bf69fffa058d5
Gayathrigeetha-Thangavel <gayathrigeetha.thangavel@gmail.com>
2025-01-16 23:44:07 +0100

    Validation message added

--- LMS.Shared/DTOs/CourseDto.cs
+++ LMS.Shared/DTOs/CourseDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace LMS.Shared.DTOs;
 public class CourseDto
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Course Name is required")]
     public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Course Description is required")]
     public string Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Start Date is required")]
     public DateTime StartDate { get; set; }
 }
